9f7e04f5f45ac0bfa53ef0072cd8af5e5430b4fe
Alex Hrin <hrinsasha@hotmail.com>
2022-01-19 17:21:00 +0200

    12

--- app/js/script.js
+++ app/js/script.js
@@ -38,32 +38,37 @@ if (menuLinks.length > 0) {
    }
 }
 
+$(document).ready(function () {
+   $('#userData').submit(function () {
+      let errors = false;
+      $(this).find('span').empty();
 
-// Отправка данных на сервер
-function send(event, php) {
-   console.log("Отправка запроса");
-   console.log(php);
-   event.preventDefault ? event.preventDefault() : event.returnValue = false;
-   var req = new XMLHttpRequest();
-   req.open('POST', php, true);
-   req.onload = function () {
-      if (req.status >= 200 && req.status < 400) {
-         json = JSON.parse(this.response); // Ебанный internet explorer 11
-         console.log(json);
-
-         // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
-         if (json.result == "success") {
-            // Если сообщение отправлено
-            alert("Сообщение отправлено");
-         } else {
-            // Если произошла ошибка
-            alert("Ошибка. Сообщение не отправлено");
+      $(this).find('input, textarea').each(function () {
+         if ($.trim($(this).val()) == '') {
+            errors = true;
+            $(this).next().text('Не заполнено поле ');
          }
-         // Если не удалось связаться с php файлом
-      } else { alert("Ошибка сервера. Номер: " + req.status); }
-   };
+      });
 
-   // Если не удалось отправить запрос. Стоит блок на хостинге
-   req.onerror = function () { alert("Ошибка отправки запроса"); };
-   req.send(new FormData(event.target));
-}
+      if (!errors) {
+         let data = $('#userData').serialize();
+         $.ajax({
+            url: 'index.php',
+            type: 'POST',
+            data: data,
+            success: function (res) {
+               if (data['error']) {
+                  alert(data['error']);
+               } else {
+                  console.log('Письмo oтврaвлeнo!');
+               }
+            },
+            error: function () {
+               alert('Ошибка');
+            }
+         });
+      }
+
+      return false;
+   });
+});
--- send.php
+++ send.php
@@ -1,41 +1,48 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
- 
-require_once '/PHPMailer/src/Exception.php';
-require_once '/PHPMailer/src/PHPMailer.php';
-require_once '/PHPMailer/src/SMTP.php';
- 
-// Для более ранних версий PHPMailer
-//require_once '/PHPMailer/PHPMailerAutoload.php';
- 
-$mail = new PHPMailer;
-$mail->CharSet = 'UTF-8';
- 
-// Настройки SMTP
-$mail->isSMTP();
-$mail->SMTPAuth = true;
-$mail->SMTPDebug = 0;
- 
-$mail->Host = 'ssl://smtp.gmail.com';
-$mail->Port = 465;
-$mail->Username = 'zakavtopidbir@gmail.com';
-$mail->Password = 'sasha22060404';
- 
-// От кого
-$mail->setFrom('zakavtopidbir@gmail.com', 'ALEX');		
- 
-// Кому
-$mail->addAddress('hrinsasha@hotmail.com', 'Sasha');
- 
-// Тема письма
-$mail->Subject = $subject;
- 
-// Тело письма
-$body = '<p><strong>«Hello, world!» </strong></p>';
-$mail->msgHTML($body);
- 
-// Приложение
-$mail->addAttachment(__DIR__ . '/image.jpg');
- 
-$mail->send();
+if($_POST){
+    // Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+    require 'PHPMailer/src/PHPMailer.php';
+    require 'PHPMailer/src/POP3.php';
+    require 'PHPMailer/src/SMTP.php';
+    require 'PHPMailer/src/Exception.php';
+
+    $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+    try {
+        //Server settings
+        $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = 'prudelnikov.viktor92@gmail.com';                 // SMTP username
+        $mail->Password = 'prudelnikov1992';                           // SMTP password
+        $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 465;                                    // TCP port to connect to
+
+        //Recipients
+        $mail->setFrom('hrinsasha@gmail.com', 'Test');
+        $mail->addAddress('info@minecash.ru', 'Владельцу ресурса MineCash.ru');     // Add a recipient
+
+
+        //Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'Новая заявка с сайта MineCash.ru';
+        $mail->Body    = ' <html>
+                    <head>
+                        <title>Новая заявка с сайта</title>
+                    </head>
+                    <body>
+                        <p>Имя: '.$_POST['name'].'</p>
+                        <p>Телефон: '.$_POST['phone'].'</p>                        
+                        <p>Сообщение: '.$_POST['message'].'</p>                        
+                    </body>
+                </html>';
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+    }
+}
